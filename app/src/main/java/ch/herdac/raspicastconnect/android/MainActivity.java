0a065bd5296601361f9c6f130228999860a06ba7
Lord Baryhobal <lordbaryhobal@gmail.com>
2022-04-02 13:01:55 +0200

    initial commit from baryhobal

--- app/src/main/java/ch/herdac/raspicastconnect/android/MainActivity.java
+++ app/src/main/java/ch/herdac/raspicastconnect/android/MainActivity.java
@@ -1,41 +1,66 @@
 package ch.herdac.raspicastconnect.android;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.net.wifi.ScanResult;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
+import android.net.NetworkSpecifier;
+import android.net.Uri;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.net.wifi.WifiNetworkSpecifier;
-import android.net.wifi.WifiNetworkSuggestion;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
 
 import java.io.IOException;
-import java.nio.channels.Channel;
+import java.io.InputStream;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String TAG= "DebugHER";
+    private static final int FILE_CHOOSER_RETURN_CODE = 1000;
+    private static final String SSH_USER = "pi";
+    private static final String SSH_HOST = "RaspiCast";
+    private static final int SSH_PORT = 22;
+    private static final String SSH_PWD = "hqum#RPI";
+    private final String TAG = "DebugHER";
     private static WifiP2pManager manager;
     private static WifiManager wmgr;
     private static WifiP2pManager.Channel channel;
     private static BroadcastReceiver receiver;
     private static IntentFilter intentFilter;
+    private static final String wifiSSID = "HERDAC";
+    private static final String wifiPWD = "hqum#wifi";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button sendFileBtn = findViewById(R.id.sendFileBtn);
+        sendFileBtn.setOnClickListener(this::sendFileClick);
+
+        /*
         manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         channel = manager.initialize(this, getMainLooper(), null);
         receiver = new WiFiDirectBroadcastReceiver(manager, channel, this);
@@ -53,9 +78,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         wmgr = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        Button btntOn = findViewById(R.id.btnOn);
-        Button btntOff = findViewById(R.id.btnOFF);
-        Button btnConnect = findViewById(R.id.connect);
+        Button btnConnect = findViewById(R.id.sendFileBtn);
         btntOn.setOnClickListener(v -> {
             Log.i(TAG, "ON");
             wmgr.setWifiEnabled(true);
@@ -74,10 +97,65 @@ public class MainActivity extends AppCompatActivity {
             conf.SSID = "HERDAC";
             wmgr.addNetwork(conf);
 
-        });
+        });*/
 
 
+    }
 
+    private void sendFileClick(View v) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            WifiConfiguration config = new WifiConfiguration();
+            config.SSID = wifiSSID;
+            config.preSharedKey = "\"" + wifiPWD + "\"";
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                makeToast("Permission denied");
+                //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACC});
+                return;
+            }
+
+            WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+            wifiManager.addNetwork(config);
+            List<WifiConfiguration> list = wifiManager.getConfiguredNetworks();
+            for (WifiConfiguration i : list) {
+                if (i.SSID != null && i.SSID.equals("\"" + wifiSSID + "\"")) {
+                    wifiManager.disconnect();
+                    wifiManager.enableNetwork(i.networkId, true);
+                    wifiManager.reconnect();
+                    break;
+                }
+            }
+
+        } else {
+            NetworkSpecifier networkSpecifier = new WifiNetworkSpecifier.Builder()
+                    .setSsid(wifiSSID)
+                    .setWpa2Passphrase(wifiPWD)
+                    .build();
+
+            NetworkRequest request = new NetworkRequest.Builder()
+                    .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                    .removeCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                    .setNetworkSpecifier(networkSpecifier)
+                    .build();
+
+            ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+            ConnectivityManager.NetworkCallback callback = new ConnectivityManager.NetworkCallback() {
+                @Override
+                public void onAvailable(Network network) {
+                    promptFileSelect();
+                }
+            };
+            connectivityManager.requestNetwork(request, callback);
+        }
+    }
+
+    private void promptFileSelect() {
+        //makeToast("Choose file");
+        Intent chooseFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        chooseFileIntent.setType("video/mp4");
+        chooseFileIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        chooseFileIntent = Intent.createChooser(chooseFileIntent, "Choose a file");
+        startActivityForResult(chooseFileIntent, FILE_CHOOSER_RETURN_CODE);
     }
 
     private boolean initP2p() {
@@ -118,13 +196,66 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        registerReceiver(receiver, intentFilter);
+        //registerReceiver(receiver, intentFilter);
     }
     /* unregister the broadcast receiver */
     @Override
     protected void onPause() {
         super.onPause();
-        unregisterReceiver(receiver);
+        //unregisterReceiver(receiver);
     }
 
+    private void makeToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == FILE_CHOOSER_RETURN_CODE) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                Uri uri = data.getData();
+                Log.i("FileChooser", String.valueOf(uri));
+
+                try {
+                    InputStream inputStream = getContentResolver().openInputStream(uri);
+                    Log.i("FileChooser", String.valueOf(inputStream.available()));
+                    sendSSH(inputStream);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //String filePath = uri.getPath();
+                //Log.i("FileChooser", filePath);
+            }
+        }
+        super.onActivityReenter(resultCode, data);
+    }
+
+    private void sendSSH(InputStream inputStream) {
+        Session session = null;
+        ChannelSftp channel = null;
+        try {
+            session = new JSch().getSession(SSH_USER, SSH_HOST, SSH_PORT);
+            session.setPassword(SSH_PWD);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+
+            channel = (ChannelSftp) session.openChannel("sftp");
+            channel.connect();
+
+            channel.put(inputStream, "/home/pi/video01.mp4");
+
+            makeToast("File sent successfully");
+
+        } catch (JSchException | SftpException e) {
+            e.printStackTrace();
+        } finally {
+            if (channel != null) {
+                channel.disconnect();
+            }
+            if (session != null) {
+                session.disconnect();
+            }
+        }
+    }
 }
